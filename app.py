a7e55d074751b524b0941b6dfd256d4dc1193aab
obz10050 <obz10050@gmail.com>
2023-08-17 14:52:45 -0400

    set Debug to true

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def main_post():
         return render_template('index.html', developer_name='Name', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
 
 
